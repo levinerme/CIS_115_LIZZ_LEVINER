cd1eb70a303ca1e24d278bca52249499af1e4a30
SANDHILLS\LH218-25 <jad.talbert@gmail.com>
2025-09-10 09:50:58 -0400

    meowmeow

---
+++ FirstNameLastName.py
@@ -0,0 +1,5 @@
+firstName = input("What's your first name? ")
+SecondName= input("What's your last name? ")
+
+FirstLast = firstName + ' ' + SecondName
+print("Your full name is {0}.".format(FirstLast))
