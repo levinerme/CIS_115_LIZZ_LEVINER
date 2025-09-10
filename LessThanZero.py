8f2bb51addde9f61bf807a266aefb53fa9e55d35
SANDHILLS\LH218-25 <jad.talbert@gmail.com>
2025-09-10 09:29:41 -0400

    week 4

---
+++ LessThanZero.py
@@ -0,0 +1,13 @@
+
+num1 = input("What's your first number? ")
+num2 = input("What's your Second number? ")
+
+sum = int(num1)-int(num2)
+
+
+if sum > 0 :
+    print("The values entered were vaild integers.")
+elif sum < 0 :
+    print("Invalid! the value is less than zero.")
+
+print =input("The total is {0}.".format(sum))
