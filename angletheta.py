0605f604ed8faf53814a3c6e8cfa7076642226a2
levinerme <levinerme058@mail.sandhills.edu>
2025-09-29 12:01:07 -0400

    Update angletheta.py

--- angletheta.py
+++ angletheta.py
@@ -4,13 +4,11 @@ def main():
     num2= float(input("Enter the length of side B: "))
     c = math.sqrt(num1*num1+num2*num2)
     print(f'The length of the hypotenuse is {c}')
-main()
-
-y= num1
-x= num2
-
-def theta():
+    y= num1
+    x= num2
     theta = math.atan2(y,x) 
     Degrees = theta * (180 / 3.14)
     print(f'The degress for the right triangle {Degrees}')
-theta()
+main()
+
+
