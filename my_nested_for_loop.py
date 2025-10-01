37d49950fe22d3ccf47de1875d5f598c90ee5b3b
SANDHILLS\LH218-25 <jad.talbert@gmail.com>
2025-10-01 09:43:04 -0400

    meow

---
+++ my_nested_for_loop.py
@@ -0,0 +1,8 @@
+def printWordList():
+    word=["Apples", "Bananas", "Pears", "Carrots"]
+
+    for word in word:
+        print(word)
+        for char in word:
+            print(char)
+printWordList()
