767952cb95e7c6b0c376560ff0776507acace6cb
levinerme <levinerme058@mail.sandhills.edu>
2025-10-02 12:11:03 -0400

    Update monthsOfYear.py

--- monthsOfYear.py
+++ monthsOfYear.py
@@ -4,8 +4,8 @@ endMonth = int(input("What's your ending month? "))
 def months_of_year(startMonth, endMonth):
     months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
     
-    if 0 <= startMonth <= 11 and endMonth <=12:
-        return months[startMonth:endMonth]
+    if 1 <= startMonth <= 12 and endMonth <=12:
+        print("months[startMonth:endMonth]")
     else:
         print("How many months do you think there are?")
     print(f"Your months are: {months_of_year(startMonth, endMonth)} ")
