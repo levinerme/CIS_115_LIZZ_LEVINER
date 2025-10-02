cd687f87f66b88f54c1b3b60b1fb1d4d1f2c332c
SANDHILLS\lh178-18 <jad.talbert@gmail.com>
2025-10-02 09:48:46 -0400

    hmm

--- kintetic_energy.py
+++ kintetic_energy.py
@@ -1,2 +1,6 @@
 def kinetic_energy(mass, velocity):
-    
+    mass = int(input("What's your mass?: "))
+    velocity = int(input("What's your velocity?: "))
+    ke = .5 * mass * (velocity ** 2)
+    print(f"Your mass is {mass}, your velocity is {velocity}, and your kentic enegry is {ke}.")
+kinetic_energy(mass, velocity)
