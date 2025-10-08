a60007541c7ea62742bb20bca303334dcdc67ed9
SANDHILLS\LH218-25 <jad.talbert@gmail.com>
2025-10-08 09:53:53 -0400

    brah

---
+++ slice_string.py
@@ -0,0 +1,9 @@
+stringVALUE= int(input("Enter a string value?: "))
+stringVALUE2 = int(input("Enter a second string value?: "))
+
+def slice_my_string(stringVALUE, stringVALUE2):
+    numbers=[0,1,2,3,4,5]
+    sliced_string = print(numbers[stringVALUE:stringVALUE2])
+    return sliced_string
+
+slice_my_string(stringVALUE, stringVALUE2)
