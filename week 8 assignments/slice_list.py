51c104aa11d6cafc1856ec1d5e7135819f161dc2
SANDHILLS\LH218-25 <jad.talbert@gmail.com>
2025-10-08 10:21:55 -0400

    guh

---
+++ week 8 assignments/slice_list.py
@@ -0,0 +1,6 @@
+
+def slice_LIST(months):
+    months=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novemeber", "Decemeber"]
+    sliced_months=months[4:6]
+    return sliced_months
+print(slice_LIST("months"))
