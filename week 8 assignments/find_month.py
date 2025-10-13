75c17f728f1756e98b4b9cd494495bf52c0985e5
levinerme <levinerme058@mail.sandhills.edu>
2025-10-12 23:27:56 -0400

    Update find_month.py

--- week 8 assignments/find_month.py
+++ week 8 assignments/find_month.py
@@ -1,9 +1,10 @@
-def search(month):
+def search_for_months():
   month = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
   search =input("What's your month?")
-
-if search in month:
+  
+  if search in month:
   print(f"we found {search} was found in the months list. Search Successful!")
-else:
-  print("We could not find {search} in months list.")
+  else:
+  print(f"We could not find {search} in months list.")
+
 
