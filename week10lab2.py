d32dd054aa29c2052e7e8edb54cfa9ea8634f0f7
SANDHILLS\LH218-25 <jad.talbert@gmail.com>
2025-10-22 10:48:46 -0400

    bruh

---
+++ week10lab2.py
@@ -0,0 +1,11 @@
+student_grades = {
+    "Alice": [85, 90, 78],
+    "Bob": [92, 88, 95],
+    "Charlie": [70, 75, 80],
+    "Diana": [100, 98, 95]
+}
+
+def caluclate_average_grade(student_grades):
+        total = student_grades
+        print(f"{total}:{average}")
+caluclate_average_grade(student_grades)
