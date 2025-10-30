efedc160285a76d3ed5ad652a6a759a443b86c4a
SANDHILLS\LH178-19 <jad.talbert@gmail.com>
2025-10-30 10:08:28 -0400

    uhgh

--- creditcardcheck.py
+++ creditcardcheck.py
@@ -8,12 +8,14 @@ for i in range (1, len(input), 2):
     input[i] *=2
 
     if input[i] >9:
-        input[i] =input[i] % 10 + 1
-    total = sum(input)
-    return total % 10 == 0    
+        input[i] =input[i] % 10 + 1  
+        total = sum(input)
+    return total % 10 == 0  
+
 
 if total == 0:
     print(f"The credit card number {ccNum} is valid!")
 else:
     print(f"Invalid credit card number {ccNum} entered. Please try again.")
+    
 validateCreditCard(ccNum)
