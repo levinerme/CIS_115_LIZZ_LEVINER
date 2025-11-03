e6e51656ac4c1196f6332a493d529a25e1aaf407
levinerme <levinerme058@mail.sandhills.edu>
2025-11-03 11:12:53 -0500

    Improve credit card validation function
    Refactor credit card validation logic for clarity and correctness.

--- creditcardcheck.py
+++ creditcardcheck.py
@@ -1,19 +1,23 @@
-ccNum =input("Enter your credit card number.")
+odd_digits = 0
+even_digits = 0
+total = 0
+
+ccNum =input("Enter your credit card number: ")
 
 def validateCreditCard(ccNum):
     ccNum = ccNum[::-1]
-    ccNum = [int(x) for x in ccNum]
-
-for i in range (1, len(ccNum)):
-    ccNum [i] *=2
-
-    if ccNum[i] >9:
-        ccNum[i] =ccNum[i] % 10 + 1  
-
-    total = sum(ccNum)
-    return(total % 10) == 0
-
-if total == 0:
+    
+    for x in ccNum[::2]:
+        odd_digits += int(x)
+        for x in ccNum[1::2]:
+            x = int(x) * 2
+            if x >= 10:
+                even_digits += (1 + ( x % 10))
+            else:
+                even_digits == x
+        total = odd_digits + even_digits
+        
+if total % 10== 0:
     print(f"The credit card number {ccNum} is valid!")
 else:
     print(f"Invalid credit card number {ccNum} entered. Please try again.")
