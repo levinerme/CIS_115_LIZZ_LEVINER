13bd59cc6beb16d2b4eed95c114279324181c705
levinerme <levinerme058@mail.sandhills.edu>
2025-11-03 16:46:10 +0000

    YAYYYYY

--- creditcardcheck.py
+++ creditcardcheck.py
@@ -1,24 +1,25 @@
-odd_digits = 0
-even_digits = 0
-total = 0
+
 
 ccNum =input("Enter your credit card number: ")
 
 def validateCreditCard(ccNum):
     ccNum = ccNum[::-1]
-    
+
+    odd_digits = 0
+    even_digits = 0
+    total = 0
     for x in ccNum[::2]:
         odd_digits += int(x)
-        for x in ccNum[1::2]:
-            x = int(x) * 2
-            if x >= 10:
-                even_digits += (1 + ( x % 10))
-            else:
-                even_digits == x
+    for x in ccNum[1::2]:
+        x = int(x) * 2
+        if x >= 10:
+            even_digits += (1 + ( x % 10))
+        else:
+            even_digits = x
         total = odd_digits + even_digits
         
-if total % 10== 0:
-    print(f"The credit card number {ccNum} is valid!")
-else:
-    print(f"Invalid credit card number {ccNum} entered. Please try again.")
-validteCreditCard(ccNum)
+    if total % 10== 0:
+      print(f"The credit card number {ccNum} is valid!")
+    else:
+        print(f"Invalid credit card number {ccNum} entered. Please try again.")
+validateCreditCard(ccNum)
