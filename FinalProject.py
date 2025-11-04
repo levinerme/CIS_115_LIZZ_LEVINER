023dd02640fcec65f288e6db5eee7a47918a7c44
SANDHILLS\lh178-18 <jad.talbert@gmail.com>
2025-11-04 10:13:07 -0500

    headers and questions

--- FinalProject.py
+++ FinalProject.py
@@ -1,5 +1,16 @@
 from tabulate import tabulate
 
+b =[
+  ["1", "USB Drive(128 GB)", "$12.00"]
+  ["2", "Mac Book Pro(15 inch)", "$2900.00",]
+  ["3", "Arduino 1010(with blue tooth)", "$48.00"]
+  ["4", "Ring Camera(wireless)", "$156.00"]
+  ["5", "Smart TV(TCL 70 inch)", "$359.00"]
+]
+headersB=["PRODUCT CATALOG"]
+
+whichID = input("Enter the product ID you would like add to your shopping cart!")
+
 a =[
   ["1", "usb_k981",	"$12.00", " ",	"USB 128 GB drive", "1000"]
   ["2",	"mbpro_490",	"$2900.00",	" ",	"Mac Book Pro 15 inch",	"45"]
@@ -7,4 +18,6 @@ a =[
   ["4",	"cam_78",	"$156.00"	 "",	"Ring Camera Model 78",	"98"]
   ["5",	"smt_tv_100",	"$359.00",	"",	"TCL Smart TV.", 	"225"]
 ]
-headers = ["Product ID", "SKU", "Price", "QTY", "Description", "Qty oh Hand"]
+headersA = ["Product ID", "SKU", "Price", "QTY", "Description", "Qty on Hand"]
+
+
