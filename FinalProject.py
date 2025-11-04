9e3d4886487fa6aa7ecdf49b6f75ea8aa2a3ff96
levinerme <levinerme058@mail.sandhills.edu>
2025-11-04 13:44:38 -0500

    Implement product catalog and shopping cart features
    Added a product catalog and user input for shopping cart functionality, including credit card validation.

--- FinalProject.py
+++ FinalProject.py
@@ -75,6 +75,32 @@ ZipCode=input("Enter your state: ")
 Email=input("Enter your Email: ")
 Phone=input("Enteryour Phone number: ")
 
+ccNum =input("Enter your credit card number: ")
+
+def validateCreditCard(ccNum):
+
+    ccNum = ccNum[::-1]
+    odd_digits = 0
+    even_digits = 0
+    total = 0
+    for x in ccNum[::2]:
+        odd_digits += int(x)
+    for x in ccNum[1::2]:
+        x = int(x) * 2
+        if x >= 10:
+            even_digits += (1 + ( x % 10))
+        else:
+            even_digits = x
+        total = odd_digits + even_digits
+        
+    if total % 10== 0:
+      print(f"The credit card number {ccNum} is valid!")
+    else:
+        print(f"Invalid credit card number {ccNum} entered. Please try again.")
+validateCreditCard(ccNum)
+
+Expiration=input("Enter the expiration date on your card:")
+CVV=input("“Please enter your CVV: ”)
 
 
 
