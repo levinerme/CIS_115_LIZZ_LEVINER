97461c2057fe291298d3f34e14c1e684f0ea1f0f
SANDHILLS\lh178-18 <jad.talbert@gmail.com>
2025-11-04 09:59:26 -0500

    table

--- FinalProject.py
+++ FinalProject.py
@@ -1,3 +1,9 @@
 from tabulate import tabulate
+
 a =[
-  [
+  ["1", "usb_k981",	"$12.00", " ",	"USB 128 GB drive", "1000"]
+  ["2",	"mbpro_490",	"$2900.00",	" ",	"Mac Book Pro 15 inch",	"45"]
+  ["3",	"chip_1010",	"$48.00", " ", "Arduino microprocessor",	"325"]
+  ["4",	"cam_78",	"$156.00"	 "",	"Ring Camera Model 78",	"98"]
+  ["5",	"smt_tv_100",	"$359.00",	"",	"TCL Smart TV.", 	"225"]
+]
