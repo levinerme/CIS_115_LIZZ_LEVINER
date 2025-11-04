c4aa49522d2f5843d3018daf741f3235c5964d4d
SANDHILLS\lh178-18 <jad.talbert@gmail.com>
2025-11-04 10:24:57 -0500

    hmmm

--- FinalProject.py
+++ FinalProject.py
@@ -11,13 +11,13 @@ headersB=["PRODUCT CATALOG"]
 
 whichID = input("Enter the product ID you would like add to your shopping cart!")
 
-a =[
-  ["1", "usb_k981",	"$12.00", " ",	"USB 128 GB drive", "1000"]
-  ["2",	"mbpro_490",	"$2900.00",	" ",	"Mac Book Pro 15 inch",	"45"]
-  ["3",	"chip_1010",	"$48.00", " ", "Arduino microprocessor",	"325"]
-  ["4",	"cam_78",	"$156.00"	 "",	"Ring Camera Model 78",	"98"]
-  ["5",	"smt_tv_100",	"$359.00",	"",	"TCL Smart TV.", 	"225"]
-]
+ProductAttributes={
+  "1", "usb_k981",	"$12.00", " ",	"USB 128 GB drive", "1000"
+  "2",	"mbpro_490",	"$2900.00",	" ",	"Mac Book Pro 15 inch",	"45"
+  "3",	"chip_1010",	"$48.00", " ", "Arduino microprocessor",	"325"
+  "4",	"cam_78",	"$156.00"	 "",	"Ring Camera Model 78",	"98]
+  "5",	"smt_tv_100",	"$359.00",	"",	"TCL Smart TV.", 	"225"
+}
 headersA = ["Product ID", "SKU", "Price", "QTY", "Description", "Qty on Hand"]
 
 
