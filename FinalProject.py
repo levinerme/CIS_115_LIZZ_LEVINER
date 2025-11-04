3c38115ae4232fccda1cd60e4bb16fa4e8de043c
levinerme <levinerme058@mail.sandhills.edu>
2025-11-04 13:22:20 -0500

    Refactor product catalog structure and display

--- FinalProject.py
+++ FinalProject.py
@@ -10,24 +10,45 @@ print("	---------------------------------------------------------------\n"\
 "	----------------------------------------------------------------")
 # Make an individualdictionary object for each product?
 usb_drive = {
-	"productID": 1,
-	"sku": "usb_k981",
-	"price": 12,
-	"quantity": 0,
-	"description": "USB 128 GB drive.",
-	"stock": 1000
+	"ProductID": 1,
+	"SKU": "usb_k981",
+	"Price": 12.00,
+	"Quantity": 0,
+	"Description": "USB 128 GB drive.",
+	"Stock": 1000
 }
-
-/*
-#Don't think that you can use this but I left it here
-b =[
-  ["1", "USB Drive(128 GB)", "$12.00"]
-  ["2", "Mac Book Pro(15 inch)", "$2900.00",]
-  ["3", "Arduino 1010(with blue tooth)", "$48.00"]
-  ["4", "Ring Camera(wireless)", "$156.00"]
-  ["5", "Smart TV(TCL 70 inch)", "$359.00"]
-]
-*/
+Mac_Book_pro = {
+	"ProductID":2,
+	"SKU":"mbpro_490",
+	"Price":2900.00,
+	"Quantity":0,
+	"Description":"Mac Book Pro 15 inch
+	"Stock":45
+}
+Arduino_1010 ={
+	"ProductID":3,
+	"SKU":"chip_1010"
+	"Price":48.00,
+	"Quantity":0,
+	"Description":"Arduino microprocessor",
+	"Stock":325
+}
+Ring_camera={
+	"ProductID":4,
+	"SKU":"cam_78",
+	"Price":156.00,
+	"Quantity":0,
+	"Description":"Ring CameraModel 78",
+	"Stock":98
+}
+Smart_TV={
+	"ProductID":5,
+	"SKU":"smt_tv_100",
+	"Price":359.00,
+	"Quantity":0,
+	"Description":"TCL Smart TV",
+	"Stock":225
+}
 
 whichID = input("Enter the product ID you would like add to your shopping cart!")
 
@@ -43,4 +64,5 @@ headersA = ["Product ID", "SKU", "Price", "QTY", "Description", "Qty on Hand"]
 #example of adding in product quanity
 #whatQTY=input("blahblahblah)
 #usb["quantity"]=(3)
+
 
